2588d621c790cf993c61725f47308e6d27a55354
PedroCunha06 <pdh.alves@gmail.com>
2024-06-21 11:10:36 -0300

    Creation model contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# ID (primary key - gerado automaticamente) -> Valor que busca na base de dados
+# first_name(string) - last_name(string) -  phone(string) - email(email) - created_date(date) - description(text)
+# DEPOIS 
+# category(foreing key) - show(boolean) - owner(foreing key) - picture(image)
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=25)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)   # Django registra automaticamente a hora de criação.
+    description = models.TextField(blank=True)
+    
+    
