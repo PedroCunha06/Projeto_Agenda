fec638fb8f9839e1caa291243021bea4b2688244
PedroCunha06 <pdh.alves@gmail.com>
2024-06-24 19:18:22 -0300

    Config search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
-
-
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('first_name')
@@ -16,6 +15,32 @@ def index(request):
         'contact/index.html',
         context
     )
+    
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')    # Quando não acha o valor, redireciona para outra página
+    
+    contacts = Contact.objects.\
+        filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |     # Usa para fazer pesquisa OU
+            Q(phone__icontains=search_value) |          # Usa para fazer pesquisa OU
+            Q(email__icontains=search_value) |          # Usa para fazer pesquisa OU
+            Q(last_name__icontains = search_value))\
+        .order_by('first_name')
+    
+    context = {
+        'contacts': contacts ,
+        'site_title': 'Contacts -'
+    }  
+    
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )   
     
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
