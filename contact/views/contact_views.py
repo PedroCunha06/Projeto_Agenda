6688e7261c6dd548d771672b3bcd185355ea018b
PedroCunha06 <pdh.alves@gmail.com>
2024-06-24 12:01:25 -0300

    Create single contact template

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+
 
 
 def index(request):
@@ -7,11 +8,23 @@ def index(request):
     
     context = {
         'contacts': contacts 
-    }
-    
+    }  
     
     return render(
         request,
         'contact/index.html',
+        context
+    )
+    
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
+    context = {
+        'contact': single_contact
+    }  
+    
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
