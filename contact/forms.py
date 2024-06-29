55df0bdb8a71c08ac134ce0b68bca1a4217e028e
PedroCunha06 <pdh.alves@gmail.com>
2024-06-29 18:01:43 -0300

    Config save form create contact

--- contact/forms.py
+++ contact/forms.py
@@ -3,25 +3,43 @@ from django import forms
 
 from . import models
 
+
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget = forms.TextInput(
+            attrs={
+                'placeholder': 'Write the first name',
+            }
+        ),
+        help_text="Don't write nickname.",
+    )
     class Meta:
         model = models.Contact
-        fields = 'first_name', 'last_name', 'description',
+        fields = 'first_name', 'last_name', 'phone', 'email', 'description', 'category',
+        
         
     def clean(self):
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        
+        if first_name == last_name:
+            self.add_error(
+                'last_name',
+                ValidationError(
+                    "The last name cannot be the same as the first. Try again",
+                    code='invalid'
+                )
             )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+        
 
-        return super().clean()
+        return cleaned_data
+        
+    def clean_first_name(self):
+            first_name = self.cleaned_data.get('first_name')
+            if first_name == 'ABC':
+                raise ValidationError(
+                    "Don't enter ABC",
+                    code = 'invalid'
+                )
+            return first_name
