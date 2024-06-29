19d425deb4cc6039a9fc80e7e1c811ba08bf9663
PedroCunha06 <pdh.alves@gmail.com>
2024-06-29 15:40:32 -0300

    Config form create contact

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from django.db.models import Q
-from django.core.paginator import Paginator
-from contact.models import Contact
+from contact.forms import ContactForm    
 
 def create(request):
+    if request.method == 'POST':
+        context = {
+        'form': ContactForm(request.POST)
+        }  
+    
+        return render(
+        request,
+        'contact/create.html',
+        context
+        )
     
     context = {
-
+        'form': ContactForm()
     }  
     
     return render(
