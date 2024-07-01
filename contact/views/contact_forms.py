c14092a9d4ac6886d76209972627559bb78859c3
PedroCunha06 <pdh.alves@gmail.com>
2024-07-01 11:04:45 -0300

    Add function delete contact

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -71,3 +71,21 @@ def update(request, contact_id):
         'contact/create.html',
         context
     )
+    
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)  # Busca do contato ou erro 404
+    confirmation = request.POST.get('confirmation', 'no')
+
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+        
+    return render (
+        request,
+        'contact/delete.html',
+        {
+            'contact':contact,
+            'confirmation': confirmation
+        }
+    )
